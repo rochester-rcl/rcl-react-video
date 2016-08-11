3bfa90eb90acff1e64de3299bea99e7811a862ed
Nathan Sarr <ndsarr@nds-mbp.local>
2016-08-11 15:58:24 -0400

    code formatting

--- example/index.jsx
+++ example/index.jsx
@@ -14,16 +14,19 @@ import VideoContainer from '../src/containers/videoContainer';
 import nleControls  from './plugins/videojs-nle-controls.min';
 
 let plugins = [
-                { plugin: nleControls,
-                  name: 'nleControls',
-                  options: { framerate: 24.0,
-                             smpteTimecode: true,
-                             frameControls: true
-                  }
-                }
-              ]
+    {
+        plugin: nleControls,
+        name: 'nleControls',
+        options: {
+            framerate: 24.0,
+            smpteTimecode: true,
+            frameControls: true
+        }
+    }
+]
 
 ReactDOM.render(
-  <VideoContainer fileUrl={'http://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_480p_h264.mov'} mime={'video/webm'} plugins={plugins}/>, // These props could also be received from a Redux store - just set the store up here
-  document.getElementById('test')
+    <VideoContainer fileUrl={'http://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_480p_h264.mov'}
+                    mime={'video/webm'} plugins={plugins}/>, // These props could also be received from a Redux store - just set the store up here
+    document.getElementById('test')
 );
